723620e5424c15aa35504eaac266cbc831feb9d0
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-13 18:42:31 -0500

    carga de tipos de variables en python

---
+++ 01_Tipos _de_variables_en_python/main.py
@@ -0,0 +1,26 @@
+# Diseño de Tipos de Variables
+print("*"*32)
+print("|       Tipos de variables      |")
+print("*"*32)
+print("- Valores Enteros (INT)=> Solo acepta numeros enteros(Ejemplo: 100, 260)")
+print("- Valores Reales (FLOAT)=> Acepta numeros enteros y con decimales (Ejemplo: 1.24, 2, 5.7)")
+print("- Cadenas de caracteres (STRING)=>Solo acepta texto (Ejemplo: 'Juan', 'Compras', 'Listado')")
+print("- Valores Booleanos (BOOL)=>Solo acepta 2 y son verdadero o false (Ejemplo: True, False)")
+
+# Declarar y mostrar ejemplos de variables
+variable_de_tipo_entero = 10
+variable_de_tipo_real = 54.8
+variable_de_tipo_caracteres = "Un saludo muy especial a todos los coders"
+variable_de_tipo_booleano = True
+
+# Diseño de Ejemplos de Variables
+print()
+print("*"*32)
+print("|     Ejemplos de variables     |")
+print("*"*32)
+print(f"Variable de tipo entero: {variable_de_tipo_entero}")
+print(f"Variable de tipo real: {variable_de_tipo_real}")
+print(f"Variable de tipo caracteres: {variable_de_tipo_caracteres}")
+print(f"Variable de tipo booleano: {variable_de_tipo_booleano}")
+
+
