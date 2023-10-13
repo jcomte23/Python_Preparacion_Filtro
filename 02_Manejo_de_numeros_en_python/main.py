cb3974c57eeb434d8a38f6f81430f455c8061236
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-13 18:56:33 -0500

    manejo de numeros en python

---
+++ 02_Manejo_de_numeros_en_python/main.py
@@ -0,0 +1,21 @@
+print("*"*41)
+print("|       Manejo de numeros en python      |")
+print("*"*41)
+
+print()
+print("Para el manejo de números, es importante distinguir entre enteros (int) y números decimales (float)")
+print("La variable 'numeroB' será un entero, lo que significa que solo puede contener números enteros, sin decimales.")
+print("La variable 'numeroC' permitirá valores decimales, lo que significa que puede contener números enteros y decimales.")
+print("La variable 'numeroB' será un entero, mientras que 'numeroC' permitirá decimales.")
+print()
+
+numeroB = int(input("Ingresa un número entero para 'numeroB': "))
+numeroC = float(input("Ingresa un número decimal para 'numeroC': "))
+
+print()
+print(f"Valor de numeroB: {numeroB}, Tipo: {type(numeroB)}")
+print(f"Valor de numeroC: {numeroC}, Tipo: {type(numeroC)}")
+
+
+
+
