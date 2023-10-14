b93f27fc0740f0574072c2370d1e4751f9ef0325
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 10:09:14 -0500

    ejercicios_multiplos de 3 y 5

---
+++ 19_Ejercicio_multiplos_de_3_y_5/main.py
@@ -0,0 +1,12 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+
+if num1 % 3 == 0 and num1 % 5 == 0:
+    print(f"{num1} es múltiplo de 3 y 5")
+
+if num2 % 3 == 0 and num2 % 5 == 0:
+    print(f"{num2} es múltiplo de 3 y 5")
+
+if num3 % 3 == 0 and num3 % 5 == 0:
+    print(f"{num3} es múltiplo de 3 y 5")
