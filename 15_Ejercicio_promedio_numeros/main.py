a2cf913333b4e67ec39a3dac4b6ca4a05c7b7a96
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 09:51:22 -0500

    ejercicio promedio numeros

---
+++ 15_Ejercicio_promedio_numeros/main.py
@@ -0,0 +1,16 @@
+print("-"*20)
+print("Promedio numeros")
+print("-"*20)
+print()
+
+numero1=float(input("Numero 1=> "))
+numero2=float(input("Numero 2=> "))
+numero3=float(input("Numero 3=> "))
+numero4=float(input("Numero 4=> "))
+numero5=float(input("Numero 5=> "))
+numero6=float(input("Numero 6=> "))
+numero7=float(input("Numero 7=> "))
+
+promedio=(numero1+numero2+numero3+numero4+numero5+numero6+numero7)/7
+
+print(f"El promedio de los numeros es: {promedio}")
