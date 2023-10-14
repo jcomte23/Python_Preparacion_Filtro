558717bd0d0881b01dcf555c3599e2866d8a563c
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 09:43:12 -0500

    conversor de temperaturas

---
+++ 13_Ejercicio_conversor_de_temperaturas/main.py
@@ -0,0 +1,7 @@
+print("-"*20)
+print("Conversor de grados")
+print("-"*20)
+print()
+celsius=float(input("grados Celsius => "))
+fahrenheit=(celsius*(9/5))+32
+print(f"{celsius}C equivalen a {fahrenheit}F")
