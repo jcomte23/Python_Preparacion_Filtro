c1f1d65a6d30be6f8160d908f5a3814a9b3c650d
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 09:34:19 -0500

    ejercicio area del triangulo

---
+++ 12_Ejercicio_area_triangulo/main.py
@@ -0,0 +1,6 @@
+base = int(input("Ingrese la base del triangulo: "))
+altura = int(input("Ingrese la altura del triangulo: "))
+
+area = int (base * altura / 2)
+
+print("El área del triangulo es: ", area)
