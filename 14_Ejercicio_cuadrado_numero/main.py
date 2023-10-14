31cc7374e55ba9e5af5a4331581438c23948b7e8
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 09:47:38 -0500

    ejercicio cuadrado de un numero

---
+++ 14_Ejercicio_cuadrado_numero/main.py
@@ -0,0 +1,9 @@
+print("-"*20)
+print("Cuadrado de numero")
+print("-"*20)
+print()
+
+numero=float(input("Ingrese un numero => "))
+cuadrado=numero**2
+
+print(f"El cuadrado de {numero} es {cuadrado}")
