029a6d51c7c8a5740ecb400d77de622b6fc19619
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 10:00:30 -0500

    ejercicio calculo edad perros

---
+++ 18_Ejercicio_edad_perros/main.py
@@ -0,0 +1,16 @@
+print()
+print(" _____________________________")
+print("|                             |")
+print("|   Calculadora edad perros   |")
+print("|_____________________________|")
+print()
+
+edadPerro=int(input("Ingrese cuantos años tiene el perro=> "))
+edadNormal=16+(edadPerro-2)*4 
+print(f"tu perro tiene {edadNormal} en años perro")
+
+print()
+print("#"*5)
+print("FIN")
+print("#"*5)
+print()
