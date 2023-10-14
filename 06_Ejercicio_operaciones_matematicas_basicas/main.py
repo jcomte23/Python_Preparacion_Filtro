116a80e756d2c3634b22b1e754bc9a4089ef58ef
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-14 08:48:05 -0500

    mejoras en el sistema

---
+++ 06_Ejercicio_operaciones_matematicas_basicas/main.py
@@ -0,0 +1,17 @@
+#algoritmo que lea dos números, calculando y escribiendo el valor de su suma, resta, producto y división.
+
+# Leer los dos números
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+# Realizar las operaciones
+suma = num1 + num2
+resta = num1 - num2
+producto = num1 * num2
+division = num1 / num2
+
+# Mostrar los resultados
+print("La suma de los números es:", suma)
+print("La resta de los números es:", resta)
+print("El producto de los números es:", producto)
+print("La división de los números es:", division)
